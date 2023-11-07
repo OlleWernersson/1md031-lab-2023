c8f5c587c74e01501ba51382e84f5882f8b5d1b2
OlleWernersson <Olle.Wernersson.3072@student.uu.se>
2023-11-07 11:59:45 +0100

    addedjs

---
+++ public/script.js
@@ -0,0 +1,33 @@
+//alert('hej')
+
+//const skills = ["Web Design","Graphic Design","SEO Optimitation","Interaction Design"];
+const skills = [
+{
+    skill: "Web Design",
+    proficiency: "Good"
+},
+{
+    skill: "Graphic Design",
+    proficiency: "Very Good"
+},
+{
+    skill: "SEO Optimazation",
+    proficiency: "Master"
+}
+]
+
+
+let myLi, mySpan;
+
+for (let skill of skills) {
+    myLi = document.createElement("li")
+    myLi.textContent = skill.skill
+    mySpan = document.createElement("span")
+    mySpan.setAttribute("class", skill.proficiency)
+    mySpan.textContent = " " + skill.proficiency
+    myLi.appendChild(mySpan)
+    document.getElementById("skills").appendChild(myLi)
+}
+
+
+
