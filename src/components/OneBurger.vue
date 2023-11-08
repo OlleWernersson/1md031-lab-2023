e46b039e7f326046692ec8f2c4399d5b3c079845
OlleWernersson <Olle.Wernersson.3072@student.uu.se>
2023-11-08 19:57:55 +0100

    vueday1

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,20 +1,83 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'OneBurger',
-    props: {
-      burger: Object
-    }
+  <div class="box a">
+    <img v-bind:src="burger.img">
+    <h3>{{burger.name}}</h3>
+    <dl>
+        <dt>Ingredients:</dt>
+        <dd>
+            <ul>
+              <li v-for="ingredient in burger.ingredients"></li>
+            </ul>
+        </dd>
+    </dl> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OneBurger',
+  props: {
+    burger: Object
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  
-  </style>
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body {
+    font-family: arial;
+    font-size: 1.2em;
+ }
+.Burgers { 
+    background-color: black;
+    color:white;
+    padding:20px;
+}
+.Burgers div {
+    padding-left: 0px;
+}
+.Burgers div h3 {
+    text-align: center;
+    margin-top: -250px;
+}
+.Burgers div img {
+    width: 250px;
+}
+.Burgers div dt {
+    margin-top: 250px;
+}
+.wrapper {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(auto-fill, 250px);
+    grid-template-rows: repeat(auto-fill, 450px);
+    background-color: black;
+    color: red; /*??*/
+    padding: 10px;
+}
+.box {
+    background-color:darkslategray;
+    color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-size: 100%;
+}
+.a {
+    /*grid-column: 1;*/
+    /*background-image: url("img/fireburger.jpg");*/
+}
+.b {
+    /*grid-column: 2;*/
+}
+.c {
+    /*grid-column:3;
+    grid-row:1;*/
+}
+#gluten {
+    font-weight: bold;
+}
+#lactose {
+    font-weight: bold;
+}
+</style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,73 @@
 <template>
-  <div>
-    <div>
-      <h1>Burgers</h1>
-      <Burger v-for="burger in burgers"
-              v-bind:burger="burger" 
-              v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-      click here
-    </div>
-  </div>
+  <header>
+  <!--    <img src="https://th.bing.com/th/id/OIG.ZE1ZhIyJQfzIPwvPELIW?w=270&h=270&c=6&r=0&o=5&pid=ImgGn" id="hi"> -->
+      <h1>Welcome to BurgerOnline</h1>
+  </header>
+  <main>
+      <section class="Burgers">
+          <h3>Select burger</h3>
+          <p>Here is where you select what burger to order</p>
+          <div class="wrapper">
+            <Burger v-for="burger in Burgers">
+            </Burger>
+          </div>
+      </section>
+      <section id="ci">
+          <h3>Customer information</h3>
+          <p>Here is where you select your informations so that we can deliver the burger</p>
+          <p>
+              <label for="firstnamelastname">Full name</label><br>
+              <input type="text" id="firstnamelastname" name="fnln" required="required" placeholder="First- and Last name">
+          </p>
+          <p>
+              <label for="email">Email</label><br>
+              <input type="email" id="email" name="em" required="required" placeholder="E-mail address">
+          </p>
+          <p>
+              <label for="Streetname">Street</label><br>
+              <input type="text" id="streetname" name="sn" required="required" placeholder="Street name">
+          </p>
+          <p>
+              <label for="House">House</label><br>
+              <input type="number" id="House" name="h" required="required" placeholder="House number">
+
+          </p>
+          <p>
+              <label for="paymentmethod">Payment Method</label><br>
+              <select id="paymentmethod" name="pm">
+                  <option selected="selected">Swish</option>
+                  <option>Card</option>
+                  <option>Crypto</option>
+                  <option>Bank</option>
+              </select>
+          </p>
+          <a>Gender</a><br>
+          <form>
+              <label>
+                  <input type="radio" name="gender" value="male">
+                  Male
+              </label>
+              <br>
+              <label>
+                  <input type="radio" name="gender" value="female">
+                  Female
+              </label>
+              <br>
+              <label>
+                  <input type="radio" name="gender" value="nonbinary">
+                  Non-Binary
+              </label>
+          </form>
+          <br>
+      </section>
+      <button type="submit">
+          <img src="https://th.bing.com/th/id/OIP.6KI4O9iZJg0ZFOPp4Nn5iQHaHa?w=190&h=190&c=7&r=0&o=5&pid=1.7">
+          Place my order!
+      </button>
+  </main>
+  <footer>
+  
+  </footer>
 </template>
 
 <script>
@@ -26,11 +84,14 @@ function MenuItem(name, KCal, url, lactose, gluten) {
   this.gluten = gluten;
 }
 
-let Burgers = [MenuItem("Fire Burger",3000,"pic",True,True),
-MenuItem("Green Burger",3000,"pic",False,False),
-MenuItem("Hallumi Burger",1000,"pic",True,True)];
+let Burgers = [
+  new MenuItem("Fire Burger", 3000, "../../public/img/fireburger.jpg", true, true),
+  new MenuItem("Green Burger", 3000, "../../public/img/greenburger.jpg", false, false),
+  new MenuItem("Hallumi Burger", 1000, "../../public/img/hallumiburger.jpg", true, true)
+];
 
-console.log(Burgers)
+console.log(Burgers); //How to use this??
+
 
 export default {
   name: 'HomeView',
@@ -39,10 +100,7 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: Burgers
     }
   },
   methods: {
@@ -64,9 +122,116 @@ export default {
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+@media screen and (max-width: 800px) {
+    h1 {
+        font-size: 6vw;
+    }
+}
+h1 {
+    font-family: 'Open Sans', sans-serif;
+    color:chocolate;
+    text-shadow: -2px -2px 0 black, 2px -2px 0 black, -2px 2px 0 black, 2px 2px 0 black;
+    font-size:40px;
+    font-style: italic;
+    text-align: center;
+    margin-top: 100px;
+}
+header {
+    background-image: url("../../public/img/header.jpg");
+    height: 200px;
+    overflow: hidden;
+}
+#hi { /*Header Image*/
+    opacity: 0.6;
+    width: 100%;
+    height: auto;
+    margin-top: -400px;
+}
+body {
+    font-family: arial;
+    font-size: 1.2em;
+ }
+.Burgers { 
+    background-color: black;
+    color:white;
+    padding:20px;
+}
+.Burgers div {
+    padding-left: 0px;
+}
+.Burgers div h3 {
+    text-align: center;
+    margin-top: -250px;
+}
+.Burgers div img {
+    width: 250px;
+}
+.Burgers div dt {
+    margin-top: 250px;
+}
+.wrapper {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(auto-fill, 250px);
+    grid-template-rows: repeat(auto-fill, 450px);
+    background-color: black;
+    color: red; /*??*/
+    padding: 10px;
+}
+.box {
+    background-color:darkslategray;
+    color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-size: 100%;
+}
+.a {
+    /*grid-column: 1;*/
+    /*background-image: url("img/fireburger.jpg");*/
+}
+.b {
+    /*grid-column: 2;*/
+}
+.c {
+    /*grid-column:3;
+    grid-row:1;*/
+}
+#gluten {
+    font-weight: bold;
+}
+#lactose {
+    font-weight: bold;
+}
+#ci {
+    /*background-color: black;
+    color:white;*/
+    border: 2px dashed black;
+    padding:20px;
+    margin-top: 10px;
+}
+li:contains('Gluten') { /* Hur får man detta att funka?*/
+    font-weight: bold;
+}
+button:hover {
+    background-color: greenyellow;
+    color:green;
+    cursor:pointer
+}
+button {
+    margin-top: 20px;
+    margin-left: 20px;
+    margin-bottom: 20px;
+}
+button img {
+  width: 20px;
+}
+section {
+    /*margin-left: 10px;*/
+}
+select {
+    background-color: pink;
+}
+.VeryGood {
+    color: green
+}
 </style>
